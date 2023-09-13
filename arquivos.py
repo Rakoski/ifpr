9581ce06967fc7f4296d68a9f99ba7604163344e
Rakoski <mastrakoski@gmail.com>
2023-09-12 21:32:14 -0300

    arquivos 2

--- arquivos.py
+++ arquivos.py
@@ -1,4 +1,14 @@
 BASE_DIR = '/home/mateus/PycharmProjects/ifpr/sim.txt'
 
+v = ["Maria\n", 'Lucas\n', 'Arthur\n']
+
+with open(BASE_DIR, "a+") as arquivo:
+    arquivo.write("sim2\n".replace("\n", ""))
+
 with open(BASE_DIR, "r") as arquivo:
-    print(arquivo.readline())
+    linha = arquivo.readlines()
+    for c in range(len(linha)):
+        print(linha[c].replace("\n", ''), end=' ' )
+
+with open(BASE_DIR, "w+") as arquivo:
+    arquivo.writelines(v)
