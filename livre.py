962f0eba81283a75ea55310f0e13c9142173bf92
Rakoski <mastrakoski@gmail.com>
2023-11-05 16:51:38 -0300

    prova ada do santander

--- livre.py
+++ livre.py
@@ -1,3 +1,18 @@
 x = 0
 if not(x == 1):
     print("nao")
+
+
+def verdadeiro(x: int, y: int) -> bool:
+    if x > 30 and not(y < 0):
+        return True
+    return False
+
+
+Entrada = [{ "nome": "Maria", "nota": 7 },{"nome": "Marta", "nota": 5 },{"nome": "Marcia", "nota": 5.5 }]
+
+print(Entrada[0]["nota"])
+reversed()
+
+
+print(verdadeiro(35, 0))
