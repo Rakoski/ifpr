0998e6c661662c2e62e3fa959f18b7d0260be810
Rakoski <mastrakoski@gmail.com>
2023-11-27 05:22:06 -0300

    Studying for IFPR's test on dictionaries part 2

--- ifpr6.py
+++ ifpr6.py
@@ -2,8 +2,15 @@
 import ifpr6_funcoes as ifpr
 
 opcao_escolhida = ifpr.menu()
-dictio = {"nome": "Márcia", "email": "marciastainer@gmail.com", "salario": 5000, "nascimento": "02/11/1972"}
+dictio = {"nome": "Mateus", "email": "msrakoski@gmail.com", "salario": 1000, "nascimento": "03/12/2004"}
 
 if opcao_escolhida == 1:
     ifpr.inserir_dados("ifpr6", dictio)
+
+elif opcao_escolhida == 2:
+    print(ifpr.ler_arquivo("ifpr6"))
+
+elif opcao_escolhida == 3:
+    print(ifpr.acessar_onde_tem_n("ifpr6", "Márcia"))
+
 
--- ifpr6_funcoes.py
+++ ifpr6_funcoes.py
@@ -19,7 +19,7 @@ def menu():
         return opcao
 
 
-def inserir_dados(arq, dicionario):
+def inserir_dados(arq, dicionario) -> None:
     arquivo = open(arq, "a")
     dicionario_como_lista = list(dicionario.values())
     for i, valores in enumerate(dicionario_como_lista):
@@ -27,7 +27,31 @@ def inserir_dados(arq, dicionario):
             arquivo.write(str(valores) + ";")
         else:
             arquivo.write(str(valores) + "\n")
-    arq.close()
+    arquivo.close()
+    return None
+
+
+def ler_arquivo(arq):
+    arquivo = open(arq, "r")
+    pra_guardar_os_dados = []
+    dados_do_arquivo = arquivo.readlines()
+    for dado in dados_do_arquivo:
+        pra_guardar_os_dados.append(dado.replace("\n", ""))
+    arquivo.close()
+    return pra_guardar_os_dados
+
+
+# Tarefa: Acessar e devolver todos os dados que contém o nome N em um arquivo
+def acessar_onde_tem_n(arq, nome):
+    arquivo = open(arq, "r")
+    pra_guardar_os_nomes = []
+    dados_do_arquivo = arquivo.readlines()
+    for dado in dados_do_arquivo:
+        splitado = dado.split(";")
+        if nome == splitado[0]:
+            pra_guardar_os_nomes.append(dado)
+    arquivo.close()
+    return pra_guardar_os_nomes
 
 
 
