b4bd95903a9f1f420eeb81595496fff111c34abc
Rakoski <mastrakoski@gmail.com>
2023-11-28 23:03:24 -0300

    Test studies 2

--- ifpr6.py
+++ ifpr6.py
@@ -1,16 +1,24 @@
 # Chaves padrão do dicionário pessoas: nome, email, sal, nasc
 import ifpr6_funcoes as ifpr
 
-opcao_escolhida = ifpr.menu()
-dictio = {"nome": "Mateus", "email": "msrakoski@gmail.com", "salario": 1000, "nascimento": "03/12/2004"}
+while True:
+    opcao_escolhida = ifpr.menu()
 
-if opcao_escolhida == 1:
-    ifpr.inserir_dados("ifpr6", dictio)
+    if opcao_escolhida == 1:
+        pessoa = ifpr.perguntar_dados()
+        ifpr.inserir_dados("ifpr6", pessoa)
 
-elif opcao_escolhida == 2:
-    print(ifpr.ler_arquivo("ifpr6"))
+    elif opcao_escolhida == 2:
+        print(ifpr.ler_arquivo("ifpr6"))
 
-elif opcao_escolhida == 3:
-    print(ifpr.acessar_onde_tem_n("ifpr6", "Márcia"))
+    elif opcao_escolhida == 3:
+        nome_pra_achar = input("Digite o nome pra buscarmos os dados: ")
+        print(ifpr.acessar_onde_tem_n("ifpr6", nome_pra_achar))
 
+    elif opcao_escolhida == 4:
+        dicionario = ifpr.ler_arquivo("ifpr6")
+        print(ifpr.listar_emails(dicionario))
+
+    elif opcao_escolhida == 5:
+        print(ifpr.listar_por_ano_nascimento("ifpr6", 2004))
 
--- ifpr6_funcoes.py
+++ ifpr6_funcoes.py
@@ -12,46 +12,93 @@ def menu():
 9. Relatório da soma de salários por nome
 10. Relatório de média de salários por idade
 11. Sair""")
+        print()
         opcao = int(input("Digite sua escolha: "))
         if not (0 < opcao <= 11):
             print("Por favor, escolha outra opção.")
             continue
         return opcao
 
+def perguntar_dados():
+    pessoa = {}
+    pessoa["nome"] = input("Nome: ")
+    pessoa["email"] = input("Email: ")
+    pessoa["salario"] = float(input("Salário: "))
+    pessoa["data_nascimento"] = input("Data de nascimento: ")
+    return pessoa
 
+
+#1
 def inserir_dados(arq, dicionario) -> None:
     arquivo = open(arq, "a")
-    dicionario_como_lista = list(dicionario.values())
-    for i, valores in enumerate(dicionario_como_lista):
-        if i < (len(dicionario_como_lista) - 1):
-            arquivo.write(str(valores) + ";")
-        else:
-            arquivo.write(str(valores) + "\n")
+    arquivo.write(f"{dicionario['nome']};{dicionario['email']};{dicionario['salario']};{dicionario['data_nascimento']}\n")
     arquivo.close()
-    return None
 
 
+#2
 def ler_arquivo(arq):
     arquivo = open(arq, "r")
-    pra_guardar_os_dados = []
-    dados_do_arquivo = arquivo.readlines()
-    for dado in dados_do_arquivo:
-        pra_guardar_os_dados.append(dado.replace("\n", ""))
+    linhas = arquivo.readlines()
+    pessoas = [""] * len(linhas)
+    for i in range(0, len(linhas)):
+        dados = linhas[i].replace("\n", "").split(";")
+        pessoas[i] = {}
+        pessoas[i]["nome"] = dados[0]
+        pessoas[i]["email"] = dados[1]
+        pessoas[i]["salario"] = float(dados[2])
+        pessoas[i]["data_nascimento"] = dados[3]
     arquivo.close()
-    return pra_guardar_os_dados
+    return pessoas
 
 
-# Tarefa: Acessar e devolver todos os dados que contém o nome N em um arquivo
+# 3
 def acessar_onde_tem_n(arq, nome):
     arquivo = open(arq, "r")
-    pra_guardar_os_nomes = []
-    dados_do_arquivo = arquivo.readlines()
-    for dado in dados_do_arquivo:
-        splitado = dado.split(";")
-        if nome == splitado[0]:
-            pra_guardar_os_nomes.append(dado)
+    linhas = arquivo.readlines()
+    pra_guardar_os_nomes = [""] * len(linhas)
+    vet = [""] * len(linhas)
+    for i in range(len(linhas)):
+        dados = linhas[i].replace("\n", "").split(";")
+        vet[i] = {}
+        vet[i]["nome"] = dados[0]
+        vet[i]["email"] = dados[1]
+        vet[i]["salario"] = float(dados[2])
+        vet[i]["data_nascimento"] = dados[3]
+        if nome in vet[i]["nome"]:
+            pra_guardar_os_nomes[i] = vet[i]
+        else:
+            continue
     arquivo.close()
     return pra_guardar_os_nomes
 
+
+# 4
+def listar_emails(dicionario):
+    pra_guardar_os_emails = [""] * len(dicionario)
+    vet = [""] * len(dicionario)
+    for i in range(len(dicionario)):
+        dados = dicionario[i]
+        vet[i] = {}
+        pra_guardar_os_emails[i] = dados["email"]
+    return pra_guardar_os_emails
+
+
+def listar_por_ano_nascimento(arq, ano_nasc):
+    arquivo = open(arq, "r")
+    linhas = arquivo.readlines()
+    pra_guardar_as_pessoas = [""] * len(linhas)
+    vet = [""] * len(linhas)
+    for i in range(len(linhas)):
+        dados = linhas[i].replace("\n", "").split(";")
+        vet[i] = {}
+        vet[i]["nome"] = dados[0]
+        vet[i]["email"] = dados[1]
+        vet[i]["salario"] = dados[2]
+        vet[i]["data_nascimento"] = dados[3]
+        if str(ano_nasc) in dados[3]:
+            pra_guardar_as_pessoas[i] = dados[i]
+        else:
+            continue
+    return pra_guardar_as_pessoas.
 
 
